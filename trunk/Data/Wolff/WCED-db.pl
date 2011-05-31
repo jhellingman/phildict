b2149919fd30220879ed57fdea478b29b4efdeb3
jhellingman@gmail.com <jhellingman@gmail.com@dc01810b-3d15-aa4c-9d3c-f643cf7e4749>
2011-05-31 05:31:30 +0000

    Cleanup DB conversion scripts.
    git-svn-id: svn://localhost@53 dc01810b-3d15-aa4c-9d3c-f643cf7e4749

--- trunk/Data/Wolff/WCED-db.pl
+++ trunk/Data/Wolff/WCED-db.pl
@@ -3,7 +3,6 @@
 use strict;
 
 use XML::LibXML;
-# use XML::LibXSLT;
 
 require Encode;
 use Unicode::Normalize;
@@ -33,13 +32,13 @@ sub main()
     open (WORD,         ">word.sql")        || die ("Could not create output file 'word.sql'");
     open (WORDENTRY,    ">wordentry.sql")   || die ("Could not create output file 'wordentry.sql'");
 
-	my $dom = XML::LibXML->load_xml(location => $infile);
-	my @entries = $dom->findnodes("/dictionary/entry");
+    my $dom = XML::LibXML->load_xml(location => $infile);
+    my @entries = $dom->findnodes("/dictionary/entry");
 
-	foreach my $entry (@entries) 
-	{
-		handleEntry($entry);
-	}
+    foreach my $entry (@entries) 
+    {
+        handleEntry($entry);
+    }
 }
 
 
@@ -50,190 +49,12 @@ sub handleEntry($)
 {
     my $entry = shift;
 
-	my $entryId = $entry->find('@id')->to_literal;
-	# print $entryId;
+    my $entryId = $entry->find('@id')->to_literal;
 
     print ENTRY "INSERT INTO `" . $prefix . "entry` VALUES ($entryId, " .
         quoteSql($entry->toString()) .
         ", " . $pageNum .
         ");\n";
-
-	# Forms
-	my @words = $entry->findnodes(".//w");
-	my $firstWord = '';
-	foreach my $word (@words) 
-	{
-		if ($firstWord eq '') 
-		{
-			$firstWord = $word->textContent;
-			# print "\n\n" . $word->textContent . "\n";
-		}
-		else
-		{
-			# print $firstWord . " + " . $word->textContent . "\n";
-		}
-	}
-}
-
-
-#
-# writeLanguageWords() -- write the collected words for a certain language to SQL statements.
-#
-sub writeLanguageWords($)
-{
-    my $lang = shift;
-    my @wordList = keys %{$wordHash{$lang}};
-
-    foreach my $word (@wordList)
-    {
-        if ($word ne "")
-        {
-            $wordId++;
-            my $normalizedWord = "";
-            if ($lang eq "ceb")
-            {
-                $normalizedWord = normalizeWolff($word);
-            }
-
-            print WORD "INSERT INTO `" . $prefix . "word` VALUES ($wordId, " .
-                quoteSql($word) . ", " .
-                quoteSql($normalizedWord) . ", " .
-                quoteSql($lang) .
-                ");\n";
-
-            my @entries = split(/ /, $wordHash{$lang}{$word});
-            foreach my $entryId (@entries)
-            {
-                if ($entryId ne "")
-                {
-                    print WORDENTRY "INSERT INTO `" . $prefix . "wordentry` VALUES ($wordId, $entryId);\n";
-                }
-            }
-        }
-    }
-}
-
-
-
-#
-# handleHeadWords() -- collect the headwords in an entry
-#
-sub handleHeadWords($)
-{
-    my $entry = shift;
-    my $remainder = $entry;
-    while ($remainder =~ /<hw>(.*?)<\/hw>/)
-    {
-        my $phrase = $1;
-        $remainder = $';
-
-        while ($phrase =~ /<s lang=\"hil\">(.*?)<\/s>/)
-        {
-            my $word = $1;
-            $phrase = $';
-
-            $word = stripXmlTags($word);
-            $word = stripPunctuation($word);
-
-            my @words = split (/ /, $word);
-            foreach my $word (@words)
-            {
-                handleWord($word, "HW", $entryId);
-            }
-        }
-    }
-}
-
-
-#
-# handleWords() -- collect all the words in an entry
-#
-sub handleWords($)
-{
-    my $entry = shift;
-
-    my $remainder = $entry;
-    while ($remainder =~ /<s lang=\"hil\">(.*?)<\/s>/)
-    {
-        my $before = $`;
-        my $phrase = $1;
-        $remainder = $';
-        handleFragmentWords($before, "EN");
-        handleFragmentWords($phrase, "CEB");
-    }
-    handleFragmentWords($remainder, "EN");
-}
-
-
-#
-# handleFragmentWords() -- collect the words in a certain language in an entry
-#
-sub handleFragmentWords($$)
-{
-    my $phrase = shift;
-    my $lang = shift;
-
-    # drop tags from phrase
-    $phrase =~s/<[a-z0-9.-]+\b(.*?)>/ /g;
-    $phrase =~s/<\/[a-z0-9.-]+>/ /g;
-
-    # drop punctuation from phrase
-    $phrase =~s/\&mdash;/ /g;
-    $phrase =~s/[".,:;?!() *[\]]+/ /g;
-    $phrase =~s/--/ /g;
-
-    # split into words
-    my @words = split(/ /, $phrase);
-
-    foreach my $word (@words)
-    {
-        handleWord($word, $lang, $entryId);
-    }
-}
-
-
-#
-# handleWord() -- store a single word in the hash with the entry ids it appears with.
-#
-sub handleWord($$$)
-{
-    my $word = shift;
-    my $lang = shift;
-    my $entryId = shift;
-
-    $word = lc($word);
-
-    if (index($wordHash{$lang}{$word}, " $entryId ") > 0)
-    {
-        # word already counted with this entry.
-        return;
-    }
-    if (!defined($wordHash{$lang}{$word}) || $wordHash{$lang}{$word} eq "")
-    {
-        # word not seen before: start new entry.
-        $wordHash{$lang}{$word} = " ";
-    }
-
-    $wordHash{$lang}{$word} .= "$entryId ";
-}
-
-
-#
-# normalizeWolff() -- normalize the spelling of Cebuano words (as written by Wolff).
-#
-sub normalizeWolff($)
-{
-    my $word = shift;
-
-    $word = lc($word);
-
-    $word =~ s/[a·‡‚A¡¿¬]/a/g;
-    $word =~ s/[eÈËÍE…» ÌÏÓIÕÃŒ]/i/g;
-    $word =~ s/[oÛÚÙO”“‘˙˘˚U⁄Ÿ€]/u/g;
-    $word =~ s/[Ò—]/n/g;
-    $word =~ s/-//g;
-
-    return $word;
 }
 
 
@@ -245,26 +66,4 @@ sub quoteSql($)
     my $field = shift;
     $field =~ s/\"/\"\"/g;
     return "\"$field\"";
-}
-
-
-#
-# stripXmlTags() -- strip XML tags from a string.
-#
-sub stripXmlTags($)
-{
-    my $string = shift;
-    $string =~ s/<\/?[a-z]+(.*?)>//g;
-    return $string;
-}
-
-
-#
-# stripPunctuation() -- strip punctuation marks from a string.
-#
-sub stripPunctuation($)
-{
-    my $string = shift;
-    $string =~ s/[.,;:()?!]/ /g;
-    return $string;
 }
--- trunk/Data/Wolff/WCED-db.xsl
+++ trunk/Data/Wolff/WCED-db.xsl
@@ -1,9 +1,15 @@
 <?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE xsl:stylesheet [
+
+<!ENTITY lf          "&#x0A;"           >
+
+]>
 
 <xsl:stylesheet
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:xs="http://www.w3.org/2001/XMLSchema"
     xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:local="localhost"
     version="2.0"
     exclude-result-prefixes="xs dc">
 
@@ -14,12 +20,12 @@
 
     <xsl:key name="id" match="*[@id]" use="@id"/>
 
+
     <xsl:template match="/">
 
         <xsl:result-document href="words.sql" method="xml" encoding="UTF-8">
-            <xsl:apply-templates mode="words" select="//f"/>
+            <xsl:apply-templates mode="words" select="//w"/>
         </xsl:result-document>
-
 
     </xsl:template>
 
@@ -31,15 +37,25 @@
             <xsl:value-of select="position()"/>
         </xsl:variable>
 
-        <xsl:text>
-INSERT INTO `wced_word` VALUES (</xsl:text>
-        <xsl:value-of select="$wordid"/><xsl:text>, </xsl:text>
-        <xsl:text>&quot;</xsl:text><xsl:value-of select="."/><xsl:text>&quot;</xsl:text>
-        <xsl:text>);</xsl:text>
+        <xsl:value-of select="local:insertWordSql(., $wordid, 'ceb')"/>
     </xsl:template>
 
 
+    <!-- INSERT INTO `wced_word` VALUES (1, "aaaa", "aaaa", "ceb"); -->
 
-<!-- INSERT INTO `kved_word` VALUES (18, "yégwa", "yigwa", "HW"); -->
+    <xsl:function name="local:insertWordSql">
+        <xsl:param name="word"/>
+        <xsl:param name="wordid"/>
+        <xsl:param name="lang"/>
+
+        <xsl:text>&lf;</xsl:text>
+        <xsl:text>INSERT INTO `wced_word` VALUES (</xsl:text>
+            <xsl:value-of select="$wordid"/>
+            <xsl:text>, </xsl:text>
+            <xsl:text>&quot;</xsl:text><xsl:value-of select="$word"/><xsl:text>&quot;</xsl:text>
+            <xsl:text>, </xsl:text>
+            <xsl:text>&quot;</xsl:text><xsl:value-of select="$lang"/><xsl:text>&quot;</xsl:text>
+        <xsl:text>);</xsl:text>
+    </xsl:function>
 
 </xsl:stylesheet>
