f9d49c6847c3f8a6b9a043f42ed980bc3c5f7494
jhellingman@gmail.com <jhellingman@gmail.com@dc01810b-3d15-aa4c-9d3c-f643cf7e4749>
2012-02-07 22:08:29 +0000

    Reviewed letter K to kugtung.
    git-svn-id: svn://localhost@94 dc01810b-3d15-aa4c-9d3c-f643cf7e4749

--- trunk/Data/Wolff/process.pl
+++ trunk/Data/Wolff/process.pl
@@ -1,12 +1,17 @@
+# process.pl -- process the Wolff dictionary.
+
 use strict;
 
 my $xsldir  = "C:\\Users\\Jeroen\\Documents\\eLibrary\\Tools\\tei2html";  # location of xsl stylesheets
 my $saxon   = "\"C:\\Program Files\\Java\\jre6\\bin\\java.exe\" -jar C:\\bin\\saxonhe9\\saxon9he.jar "; # (see http://saxon.sourceforge.net/)
 
-my $filename = $ARGV[0];
-my $stylesheet = $ARGV[1];
+my $letter = $ARGV[0];
 
-
+if ($letter ne '') 
+{
+	processLetter($letter);
+	exit;
+}
 
 # system ("perl WCED-uptag1.pl part6.tei > part6-ut.tei");
 # system ("perl -S tei2html.pl -x part6-ut.tei");
