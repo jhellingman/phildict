1835aff7dc83cb94de1edde6432014c65971bc76
jhellingman@gmail.com <jhellingman@gmail.com@dc01810b-3d15-aa4c-9d3c-f643cf7e4749>
2012-05-04 08:35:21 +0000

    Fixing various issues found by checking wordlist upto dánag.
    git-svn-id: svn://localhost@156 dc01810b-3d15-aa4c-9d3c-f643cf7e4749

---
+++ trunk/Data/Wolff/numberParagraphs.pl
@@ -0,0 +1,32 @@
+# numberParagraphs.pl -- number the paragraphs in a document.
+
+
+use strict;
+use Roman;          # Roman.pm version 1.1 by OZAWA Sakuro <ozawa@aisoft.co.jp>
+
+my $inputFile   = $ARGV[0];
+my $parNumber   = $ARGV[1];
+
+open(INPUTFILE, $inputFile) || die("Could not open $inputFile");
+
+while (<INPUTFILE>)
+{
+    my $line = $_;
+
+    my $remainder = $line;
+
+    while ($remainder =~ m/<p\b(.*?)>/)
+    {
+        my $before = $`;
+        my $attrs = $1;
+        $remainder = $';
+        $parNumber++;
+
+        $attrs = $attrs . " id=$parNumber";
+
+        print "$before<p$attrs>";
+    }
+    print $remainder;
+}
+
+print STDERR $parNumber;
