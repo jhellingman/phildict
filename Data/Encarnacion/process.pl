ae34cfdb0713a661352ec0121ec357d0f321db7f
Jeroen Hellingman <jeroen@bohol.ph>
2023-01-26 22:14:14 +0100

    More edits; integrate missing part into main TEI file.

--- Data/Encarnacion/process.pl
+++ Data/Encarnacion/process.pl
@@ -2,5 +2,12 @@
 
 system("perl identry.pl EspanolBisaya-0.1.tei > edeb-0.2.tei");
 system("perl refvide.pl edeb-0.2.tei > edeb-0.3.tei");
-system("tei2html edeb-0.3.tei");
+system("tei2html -u edeb-0.3.tei");
 
+system("tei2html -k --kwicvariants=3 edeb-0.3.tei");
+
+# system("tei2html -k --kwicmixup=\"in m ni\" --kwicvariants=3 edeb-0.3.tei");
+# system("tei2html -k --kwicmixup=\"u n\" --kwicvariants=3 edeb-0.3.tei");
+# system("tei2html -k --kwicmixup=\"u o\" --kwicvariants=3 edeb-0.3.tei");
+# system("tei2html -k --kwicmixup=\"c o\" --kwicvariants=3 edeb-0.3.tei");
+# system("tei2html -k --kwicmixup=\"s c\" --kwicvariants=3 edeb-0.3.tei");
