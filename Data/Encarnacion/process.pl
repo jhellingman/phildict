b7f3bc8dc924d8221d5d4d26b74a3cdc4699b202
Jeroen Hellingman <jeroen@bohol.ph>
2023-01-28 19:37:01 +0100

    Encarnacion: more checks and fixes.

--- Data/Encarnacion/process.pl
+++ Data/Encarnacion/process.pl
@@ -4,7 +4,9 @@ system("perl identry.pl EspanolBisaya-0.1.tei > edeb-0.2.tei");
 system("perl refvide.pl edeb-0.2.tei > edeb-0.3.tei");
 system("tei2html -u edeb-0.3.tei");
 
-system("tei2html -k --kwicvariants=3 edeb-0.3.tei");
+system("perl -S ucwords.pl -r EspanolBisaya.xml > words-retro.html");
+
+# system("tei2html -k --kwicvariants=3 edeb-0.3.tei");
 
 # system("tei2html -k --kwicmixup=\"in m ni\" --kwicvariants=3 edeb-0.3.tei");
 # system("tei2html -k --kwicmixup=\"u n\" --kwicvariants=3 edeb-0.3.tei");
